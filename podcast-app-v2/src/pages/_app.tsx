44a5ab961b6d9ea5603d42d47f4492364f23f55c
Leonardo <leozikasantos73@gmail.com>
2021-05-31 13:49:54 -0300

    podcaster app

---
+++ podcast-app-v2/src/pages/_app.tsx
@@ -0,0 +1,33 @@
+
+import Header from '../components/Header';
+import Player from '../components/Player';
+
+import '../styles/global.scss';
+import styles from '../styles/app.module.scss'
+
+import type { AppProps } from 'next/app'
+import { PlayerContextProvider } from '../contexts/PlayerContext';
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+
+  return (
+      <PlayerContextProvider>
+
+        <div className={styles.wrapper}>
+          <main>
+            <Header/>
+            <Component {...pageProps} />
+          </main>
+          <Player/>
+        </div>
+
+      </PlayerContextProvider>
+      
+
+  )
+}
+export default MyApp;
+
+
+
